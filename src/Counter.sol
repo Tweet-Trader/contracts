1d8c78b8d28f049a024802256658ccaa8c61fdd2
0xNahhh <0xNahhh@gmail.com>
2023-08-12 11:11:18 +0900

    feat: Initial changes for implementing uniswap v2 router

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.21;
 
+import {IERC20} from "openzeppelin/interfaces/IERC20.sol";
+
 contract TwitterBot {
-    function buyTokens(address tokenAddress) external {}
+    function buyTokens(address tokenAddress) external {
+        IERC20 token = IERC20(tokenAddress);
+
+        tokenAdress
+    }
 
     function sellTokens(address tokenAddress) external {}
 }
